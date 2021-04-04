644614322d14b04d1dc435088eaa88f7a087ffcc
mochang2 <qjsrodksro@gmail.com>
2021-04-05 00:38:54 +0900

    scroll on agreement-content

--- toyproject/UnivMaps/users/static/users/js/signup.js
+++ toyproject/UnivMaps/users/static/users/js/signup.js
@@ -5,10 +5,18 @@
 
     function termsListener() {
         const hideShowTerms = document.querySelector(".hide-and-show-terms")
+        const faArrow = document.querySelector(".fa-arrow-down")
+        const agreementContent = document.querySelector(".agreement-content")
 
         hideShowTerms.addEventListener("click", (Event) => {
-            console.log(1)
-            console.log(Event.type)
+            if (faArrow.style.transform !== "rotate(0.5turn)") {
+                faArrow.style.transform = "rotate(0.5turn)"
+                agreementContent.style.display = "block"
+            }
+            else if (faArrow.style.transform == "rotate(0.5turn)") {
+                faArrow.style.transform = "rotate(1turn)"
+                agreementContent.style.display = "none"
+            }
         })
     }
 
