a93f32bffc0076207c6032c8734c082f06cc2bd3
mochang2 <qjsrodksro@gmail.com>
2021-04-14 00:15:14 +0900

    added password validation rule on signup.js2

--- toyproject/UnivMaps/users/static/users/js/signup2.js
+++ toyproject/UnivMaps/users/static/users/js/signup2.js
@@ -1,19 +1,38 @@
 (function (window, document, undefined) {
     window.onload = function () {
-        //goback()
+        goback()
         active()
+        checkPassword()
     };
 
-    // function goback() {
-    //     const goBackBtn = document.querySelector(".goback-btn")
-    //     goBackBtn.addEventListener("click", (Event) => {
-    //         location.href = "/hanyang_map"
-    //     })
-    // }
+    function goback() {
+        const goBackBtn = document.querySelector(".goback-btn")
+        goBackBtn.addEventListener("click", (Event) => {
+            location.href = "/hanyang_map"
+        })
+    }
 
     function active() {
         const stepSingLi = document.querySelectorAll("li")[1]
         stepSingLi.classList.add("active")
+    }
+
+
+    function checkPassword() {
+        const rule = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,20}$/
+        let inputtxt = document.getElementById("signup-password")
+        const warningPasswordValidation = document.querySelector(".warning-password-validation")
+
+        inputtxt.addEventListener("focusout", (Event) => {
+            if (inputtxt.value.match(rule)) {
+                warningPasswordValidation.style.display = "none"
+            }
+            else {
+                warningPasswordValidation.style.display = "block"
+                inputtxt.focus()
+            }
+        })
+
     }
 
 })(window, document, undefined);
