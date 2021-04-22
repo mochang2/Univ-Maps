875a542cb6b3f035bbb01784cc568ccc1ac5ef94
mochang2 <qjsrodksro@gmail.com>
2021-04-22 22:12:59 +0900

    updated readypage

---
+++ toyproject/UnivMaps/readypage/static/readypage/js/readypage.js
@@ -0,0 +1,33 @@
+(function (window, document, undefined) {
+    window.onload = function () {
+        //우클릭 방지 + 더블클릭 방지 + 드래그 방지
+        window.document.oncontextmenu = new Function("return false")
+        window.document.onselectstart = new Function("return false")
+        window.document.ondragstart = new Function("return false")
+
+        preventCtrlSU()
+    }
+
+    function preventCtrlSU() {
+        window.addEventListener("keydown", (Event) => {
+            let keycode
+            if (Event.key !== undefined) {
+                keycode = Event.key
+            }
+            else if (Event.keyIdentifier !== undefined) {
+                keycode = Event.keyIdentifier
+            }
+            else if (Event.keyCode !== undefined) {
+                // keyCode는 곧 사라짐. 이렇게 하면 모두 대문자로 변환됨.
+                keycode = String.fromCharCode(Event.keyCode)
+            }
+
+            // ctrl + s, ctrl + u 방지
+            if (keycode == "Control" || keycode == "s" || keycode == "u") {
+                Event.preventDefault()
+                Event.returnValue = false
+            }
+        })
+    }
+
+})(window, document, undefined)
--- toyproject/UnivMaps/readypage/views.py
+++ toyproject/UnivMaps/readypage/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "readypage/index.html")
+    return render(request, "readypage/readypage.html")
